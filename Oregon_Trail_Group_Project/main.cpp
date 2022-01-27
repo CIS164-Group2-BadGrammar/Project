0be3862b997b779319f5a43d96f70bc778e2f88d
Reed James <reedtjames1234@gmail.com>
2022-01-27 11:55:32 -0600

    initial project created

---
+++ Oregon_Trail_Group_Project/main.cpp
@@ -0,0 +1,18 @@
+// Andrew Steele, Reed James, Benjamin Lehmann, Calli Herr, Ryan Brockman
+// Setup:
+// Description: Text adventure game based off oregon trail.
+
+#include <vector>
+#include <array>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+using namespace std;
+using std::vector;
+
+int main()
+{
+    cout << "Hello World" << endl;
+
+    return 0;
+}
