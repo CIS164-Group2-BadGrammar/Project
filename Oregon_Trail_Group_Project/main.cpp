1347d6fa7b85cf6d6786b46da5dea4ef27ce13f3
Reed James <reedtjames1234@gmail.com>
2022-02-07 17:21:20 -0600

    code cleanup

--- Oregon_Trail_Group_Project/main.cpp
+++ Oregon_Trail_Group_Project/main.cpp
@@ -15,7 +15,6 @@ using namespace std;
 int main()
 {
 
-//    srand (15);
     int randNumResult;
     int numRange = 100;
     int negPosRange = 60;
@@ -27,40 +26,6 @@ int main()
     cout <<endl;
     test1.display();
     cout << "Random Num" << randNumResult << endl;
-
     return 0;
 }
 
-
-//Random Generator
-
-
-
-//Scrapyard
-/*
- *
-
-    for(int i=0; i<top;i++){
-        randNum = (rand()% numRange) -negPosRange;
-        if(randNum > max){
-            max = randNum;
-        }
-        if(randNum < min){
-            min = randNum;
-        }
-        cout << "  " << randNum;
-        if(i==(top/2)){
-            cout<<endl;
-        }
-    }
-
-    cout << "\nmax: " << max << endl;
-    cout << "min: " << min << endl << endl;
-
-
-
-
-
-
-
- */
