eff030af5a605774f6ec41e5a4a206a4e780b3a2
BenLehmann5 <92688639+BenLehmann5@users.noreply.github.com>
2022-02-09 18:28:30 -0600

    Create a User Class for States

---
+++ Oregon_Trail_Group_Project/User.h
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class User {
+    public:
+    int miles,pound,clothes,ammo,spare,food,group,money,updateMiles,updatePound,updateClothes,updateAmmo,updateSpare,updateFood,updateGroup,updateMon;
+    string state;
+
+    //Setters
+    void setMiles(int);
+    void setPound(int);
+    void setClothes(int);
+    void setAmmo(int);
+    void setSpare(int);
+    void setFood(int);
+    void setGroup(int);
+    void setMoney(int);
+
+    string getState();
+    int getMiles();
+
+    virtual bool challenge() = 0;
+
+    int upMiles();
+    int upPound();
+    int upClothing();
+    int upAmmo();
+    int upSpare();
+    int upFood();
+    int upGrouping();
+    int upMoney();
+};
